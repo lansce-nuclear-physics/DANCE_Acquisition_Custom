e5c95b9336ae020da6ea759be65396f2876422c9
Christopher J. Prokop <cprokop@dance-cruncher1.lanl.gov>
2018-04-05 18:17:45 -0600

    Version 2.1.3 - Changed the hardcoded eventid for digitizer and diagnositcs to pull from eventid.h.  EventID remains 1 and 8 for digitizer and diagnostics respectively.

--- eventid.h
+++ eventid.h
@@ -1,18 +1,1 @@
-#ifndef EVENTIDH
-  #define EVENTIDH
-
-  #define DIGIT_EVENTID  1
-  #define SCALER_EVENTID   2
-  #define HV_EVENTID  3
-  //
-  #define CAMAC_EVENTID 4
-  // We define the VME_EVENTID the same as CAMAC.  
-  // Not sure this is smart--fortunately, they don't go into the 
-  // data stream at this time.
-  #define VME_EVENTID 4
-
-  #define NTP_EVENTID 5
-  #define SMART_EVENTID 6
-  #define ELOG_EVENTID 7
-
-#endif
+../eventid.h
